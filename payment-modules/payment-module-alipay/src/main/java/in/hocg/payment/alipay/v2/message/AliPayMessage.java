5ff794eb7afc9581da7f352332316925e39972e0
hocgin <hocgin@gmail.com>
2021-01-06 23:02:11 +0800

    # 银联支付集成

--- payment-modules/payment-module-alipay/src/main/java/in/hocg/payment/alipay/v2/message/AliPayMessage.java
+++ payment-modules/payment-module-alipay/src/main/java/in/hocg/payment/alipay/v2/message/AliPayMessage.java
@@ -1,19 +1,18 @@
 package in.hocg.payment.alipay.v2.message;
 
 import com.google.common.collect.Lists;
-import com.google.common.collect.Maps;
+import in.hocg.payment.PaymentMessage;
 import in.hocg.payment.alipay.Helpers;
 import in.hocg.payment.alipay.v2.AliPayConfigStorage;
 import in.hocg.payment.alipay.v2.AliPayService;
-import in.hocg.payment.PaymentMessage;
 import in.hocg.payment.sign.ApiField;
 import in.hocg.payment.sign.SignType;
 import in.hocg.payment.sign.SignValue;
+import in.hocg.payment.utils.MapUtils;
 import in.hocg.payment.utils.ObjectMeta;
 import lombok.Getter;
 
-import java.net.URLDecoder;
-import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -30,37 +29,27 @@ public abstract class AliPayMessage extends PaymentMessage<AliPayService> {
     @ApiField(value = "sign_type", ignore = true)
     @ObjectMeta.Alias("sign_type")
     private String signType;
-    
+
     private boolean checkSign() {
         String body = getContent();
-        final String[] items = body.split("&");
-        Map<String, Object> values = Maps.newHashMap();
-        final ArrayList<String> ignoreKeys = Lists.newArrayList("sign", "sign_type");
-        String[] vars;
-        for (String item : items) {
-            vars = item.split("=", 2);
-            final String key = vars[0];
-            if (ignoreKeys.contains(key)) {
-                continue;
-            }
-            values.put(key, URLDecoder.decode(vars[1]));
-        }
-    
+        final List<String> ignoreKeys = Lists.newArrayList("sign", "sign_type");
+        Map<String, Object> values = MapUtils.getValues(body, ignoreKeys);
+
         final AliPayConfigStorage configStorage = this.getService().getConfigStorage();
         String publicKey = configStorage.getAliPayPublicKey();
         SignValue signValue = Helpers.newSignValue().handle(values);
         String data = signValue.getSignValue();
         return SignType.valueOf(getSignType()).verify(data, publicKey, getSign());
     }
-    
+
     @Override
     public void afterPropertiesSet() {
         if (!checkSign()) {
             throw new RuntimeException("签名错误");
         }
     }
-    
+
     public interface Result extends PaymentMessage.Result {
-    
+
     }
 }
--- payment-modules/payment-module-chinaums/src/main/java/in/hocg/payment/chinaums/convert/ChinaUmsConverts.java
+++ payment-modules/payment-module-chinaums/src/main/java/in/hocg/payment/chinaums/convert/ChinaUmsConverts.java
@@ -1,13 +1,12 @@
 package in.hocg.payment.chinaums.convert;
 
 import com.alibaba.fastjson.JSON;
-import com.google.common.collect.Maps;
 import in.hocg.payment.chinaums.v4_8.message.ChinaUmsPayMessage;
 import in.hocg.payment.chinaums.v4_8.response.ChinaUmsPayResponse;
 import in.hocg.payment.convert.StringConvert;
+import in.hocg.payment.utils.MapUtils;
 import in.hocg.payment.utils.ObjectMeta;
 
-import java.net.URLDecoder;
 import java.util.Map;
 
 /**
@@ -24,18 +23,7 @@ public class ChinaUmsConverts {
         public <R extends ChinaUmsPayMessage> R convert(String body, Class<R> clazz) {
             try {
                 final R result = clazz.newInstance();
-                final String[] items = body.split("&");
-                Map<String, String> params = Maps.newHashMap();
-                String[] vars;
-                for (String item : items) {
-                    vars = item.split("=", 2);
-                    final String key = vars[0];
-                    String value = vars[1];
-                    if (!"sign".equalsIgnoreCase(key)) {
-                        value = URLDecoder.decode(value);
-                    }
-                    params.put(key, value);
-                }
+                Map<String, Object> params = MapUtils.getValues(body);
 
                 final ObjectMeta objectMeta = ObjectMeta.from(clazz);
                 params.forEach((key, value) -> {
